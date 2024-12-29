0d6c5c3fe980b4a2b9eb1d9422daf6787382a2c9
ZunairaSilma42 <148058662+ZunairaSilma42@users.noreply.github.com>
2024-12-29 14:58:22 +0700

    zunaira_product

---
+++ database/migrations/2024_12_27_013358_add_categories_id_to_product_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('category');
+            $table->foreignId('category_id')->constrained('categories')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('product', function (Blueprint $table) {
+            //
+        });
+    }
+};
